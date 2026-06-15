1d044697e85c6f3be9bbb44cb3c068cdf6f6d5fd
Yong Xu <134538561+xy0305@users.noreply.github.com>
2026-06-15 21:36:24 +0800

    Update ListenSoulVIP.js

--- ListenSoulVIP.js
+++ ListenSoulVIP.js
@@ -1,7 +1,7 @@
 let body = $response.body;
 
-body = body.replace('"vip_status":false', '"vip_status":true');
-body = body.replace('"max_devices":1', '"max_devices":999');
-body = body.replace('"vip_time":null', '"vip_time":"2099-12-31T23:59:59.000Z"');
+body = body.replace(/"vip_status":false/g, '"vip_status":true');
+body = body.replace(/"max_devices":1/g, '"max_devices":999');
+body = body.replace(/"vip_time":null/g, '"vip_time":"2099-12-31T23:59:59.000Z"');
 
 $done({ body });
